d313febdfe316db4401699ac32fdb8ab10f2fc17
expinc <expinc@outlook.com>
2019-11-20 22:07:51 +0800

    Implement text file writing

--- src/infra/FileIo.cpp
+++ src/infra/FileIo.cpp
@@ -20,26 +20,40 @@ namespace StupidPoet
         long length = ftell(file);
         fseek(file, 0, SEEK_SET);
         char*   utf8Content = new char[length + 1];
-        fread(utf8Content, sizeof(char), length, file);
+        size_t  readCount = fread(utf8Content, sizeof(char), length, file);
         utf8Content[length] = '\0';
         fclose(file);
         file = nullptr;
 
         // Convert content
-        if (utf8Content)
+        if (utf8Content && readCount == length)
         {
             dst = UStr::fromUtf8(utf8Content);
             delete[] utf8Content;
             utf8Content = nullptr;
+            return true;
         }
-        return true;
+        return false;
     }
 
 
-    bool WriteTextToFile(const UChar* path, UStr& src)
+    bool WriteTextToFile(const UChar* path, const UChar* content)
     {
-        // TODO
-        return false;
+        if (nullptr == path || nullptr == content)
+            return false;
+
+        std::string pathUtf8 = UStr::toUtf8(path);
+        std::string contentUtf8 = UStr::toUtf8(content);
+        FILE*   file = fopen(pathUtf8.c_str(), "wb");
+        if (nullptr != file)
+        {
+            int error = fputs(contentUtf8.c_str(), file);
+            fclose(file);
+            file = nullptr;
+            if (EOF == error)
+                return false;
+        }
+        return true;
     }
 
 
--- src/infra/FileIo.hpp
+++ src/infra/FileIo.hpp
@@ -9,6 +9,6 @@
 namespace StupidPoet
 {
     bool ReadTextFromFile(const UChar* path, UStr& dst);
-    bool WriteTextToFile(const UChar* path, UStr& src);
+    bool WriteTextToFile(const UChar* path, const UChar* content);
     std::shared_ptr<std::list<UStr>> GetFileList(const UChar* path);
 }
